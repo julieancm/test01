8b396ccf523dc7d601b1bc3501ceb3f3f6aeee13
julie <jmanahan@sunloft.co.jp>
2014-02-05 11:16:29 +0900

    1回目のコミット複数のファイル追加

---
+++ page01.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+<head>
+	<title>Untitled</title>
+</head>
+
+<body>
+
+<?php
+
+print "Hello World!Hello World!Hello World!Hello World!Hello World!";
+print "Hello World!Hello World!Hello World!Hello World!Hello World!";
+print "Hello World!Hello World!Hello World!Hello World!Hello World!";
+print "Hello World!Hello World!Hello World!Hello World!Hello World!";
+
+
+?>
+
+</body>
+</html>
---
+++ page02.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+<head>
+	<title>Untitled</title>
+</head>
+
+<body>
+
+<?php
+
+print "日本語文字";
+print "日本語文字";
+print "日本語文字";
+print "日本語文字";
+print "日本語文字";
+print "日本語文字";
+print "日本語文字";
+print "日本語文字";
+?>
+
+</body>
+</html>
